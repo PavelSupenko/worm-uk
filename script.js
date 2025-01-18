4524f1fb785bc691c912bb937263182e62532af4
Pavlo Supenko <pavlo.supenko@bini.games>
2025-01-18 20:12:07 +0200

    Add ability to change character names on fly

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const names = JSON.parse(localStorage.getItem('customNames')) || {};
 
 // Завантаження перекладів за замовчуванням
-fetch('assets/translations.json')
+fetch('../assets/translations.json')
     .then(response => response.json())
     .then(defaultNames => {
         // Об'єднати імена з LocalStorage та початкових даних
         Object.assign(names, defaultNames);
+        replaceNamesInText();
     });
 
 // Редагування імен
@@ -17,9 +18,19 @@ document.getElementById('edit-names').addEventListener('click', () => {
             const parsedNames = JSON.parse(userNames);
             Object.assign(names, parsedNames);
             localStorage.setItem('customNames', JSON.stringify(names));
+            replaceNamesInText();
             alert("Імена збережено!");
         } catch (e) {
             alert("Помилка в форматі JSON!");
         }
     }
 });
+
+// Функція для заміни імен у тексті
+function replaceNamesInText() {
+    document.querySelectorAll('[data-name]').forEach(element => {
+        const originalName = element.dataset.name;
+        const translation = names[originalName]?.singular?.nominative || originalName;
+        element.textContent = translation;
+    });
+}
